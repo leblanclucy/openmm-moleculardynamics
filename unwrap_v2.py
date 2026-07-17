4dd41df864a4aea53fcdb87f6f0daab0595b6676
Lucy LeBlanc <lucyleblanc@utexas.edu>
2026-07-17 14:06:32 -0500

    Update unwrap_v2.py

--- unwrap_v2.py
+++ unwrap_v2.py
@@ -1,6 +1,8 @@
 import mdtraj as md
 
 # loads the trajectory and solvated_system file then centers the trajectory so that the complex no longer appears as though it is 'teleporting' when it bumps against the periodic boundary conditions.
+# note that although it is possible to unwrap the trajectory.dcd file on the fly in the mdanalysis.py script, there were inconsistencies with physically impossible radial distribution function values (RDF). 
+# other readouts like RMSD were unaffected. thus it is highly recommended to unwrap before analysis (and it's required before visualization).
 print("Loading trajectory + topology file.")
 traj = md.load('trajectory.dcd', top='solvated_system.pdb')
 
